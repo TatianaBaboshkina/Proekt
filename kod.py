f3397b0730cd9d2e1c97eb66d901b12f9ddc9484
TatianaBaboshkina <79750821+TatianaBaboshkina@users.noreply.github.com>
2021-06-09 13:57:58 +0300

    Add files via upload
    Финальный вариант кода, работает с файлом Excel "oko_2", скомпилирован в исполнительный файл :)

--- kod.py
+++ kod.py
@@ -1,20 +1,9 @@
 import openpyxl
+from tkinter import *
 
 wb_obj = openpyxl.load_workbook("oko_2.xlsx")
 sheet_obj = wb_obj.active
 
-"""
-cell_obj = sheet_obj.cell(row = 3, column = 2)
-kriteri = sheet_obj.cell(row = 3, column = 1)
-  pokazatel = sheet_obj.cell(row = i, column = 1)
-    print(pokazatel.value)
-    print(str(pokazatel.value) + ' - ' )
-    i = i+1
-print(cell_obj.value) - названия столбцов
-print(sheet_obj.max_row) - количество строк
-print(sheet_obj.max_column) - количество столбцов
-"""
-#Привязана динамическая таблица с автоматическим обновлением значений
 #m - максимальное количество строк
 #c - максимальное количество столбов
 #ssred - сумма показателей по строке
@@ -30,6 +19,9 @@ c = sheet_obj.max_column
 ssred = 0
 sred=0
 b=0
+o = 0
+window = Tk()
+window.title("Удовлетворённость клиентов ДЦ ГАЗ")
 while j < c and i<m:
     znach = sheet_obj.cell(row = i, column = j)
     pokazatel = sheet_obj.cell(row = i, column = 1)
@@ -44,35 +36,9 @@ while j < c and i<m:
         ssred = 0
         b = 0
         print(str(pokazatel.value) + " " + str(sred))
-    
-    
-    
-    
-    
-'''    
-print(pokazatel)
-while i < m: 
-    pokazatel = sheet_obj.cell(row = i, column = 3)
-    kriteri = sheet_obj.cell(row = i, column = 1)
-    print(pokazatel.value)
-    #print(str(kriteri.value) + ' - ' + str(pokazatel.value))
-    i = i+1
+        lbl = Label(window, text = str(pokazatel.value) + " - " + str(sred))   
+        lbl.grid(column=0, row=o)
+        o = o + 1
+window.mainloop()
 
-"""
-import pandas as pd
-import matplotlib.pyplot as plt
-import matplotlib
-matplotlib.style.use('ggplot')
-
-#подставьте ссылку на ваш файл или полный путь к файлу на вашем компьютере...    
-url = 'https://docs.google.com/spreadsheet/ccc?key=0Ak1ecr7i0wotdGJmTURJRnZLYlV3M2daNTRubTdwTXc&output=csv'
-
-df = pd.read_csv(url, names=['val','date'], index_col=[1], decimal=',',
-                 parse_dates=True, dayfirst=True)
-
-df.plot()
-#plt.savefig('d:/temp/out.png')
-plt.show()
-"""
-
-'''
+    
